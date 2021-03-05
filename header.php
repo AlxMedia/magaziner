0cd8c43f84c84fe207aa49c7ef40281842a271a6
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-03-05 17:09:53 +0100

    3.1.1

--- header.php
+++ header.php
@@ -34,13 +34,13 @@
 					
 					<?php if ( has_nav_menu('mobile') ): ?>
 						<div id="wrap-nav-mobile" class="wrap-nav">
-							<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'mobile','menu_id' => 'nav-mobile','fallback_cb'=> false)); ?>
+							<?php \Magaziner\Nav::nav_menu(array('theme_location'=>'mobile','menu_id' => 'nav-mobile','fallback_cb'=> false)); ?>
 						</div>
 					<?php endif; ?>
 					
 					<?php if ( has_nav_menu('topbar') ): ?>
 						<div id="wrap-nav-topbar" class="wrap-nav">
-							<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'topbar','menu_id' => 'nav-topbar','fallback_cb'=> false)); ?>
+							<?php \Magaziner\Nav::nav_menu(array('theme_location'=>'topbar','menu_id' => 'nav-topbar','fallback_cb'=> false)); ?>
 						</div>
 					<?php endif; ?>
 					
@@ -86,7 +86,7 @@
 						
 						<?php if ( has_nav_menu('header') ): ?>
 							<div id="wrap-nav-header" class="wrap-nav">
-								<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'header','menu_id' => 'nav-header','fallback_cb'=> false)); ?>
+								<?php \Magaziner\Nav::nav_menu(array('theme_location'=>'header','menu_id' => 'nav-header','fallback_cb'=> false)); ?>
 								
 								<?php if ( get_theme_mod( 'header-search', 'on' ) == 'on' ): ?>
 									<div class="search-trap-focus">
