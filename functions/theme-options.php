2b6ac38550ddd1f6f5ffdd64e2a799ccac1b9017
AlxMedia <contact@alx.media>
2022-02-23 12:06:29 +0100

    Update FontAwesome customizer link to v5 brands

--- functions/theme-options.php
+++ functions/theme-options.php
@@ -725,7 +725,7 @@ Kirki::add_field( 'magaziner_theme', array(
 	'label'			=> esc_html__( 'Create Social Links', 'magaziner' ),
 	'description'	=> esc_html__( 'Create and organize your social links', 'magaziner' ),
 	'section'		=> 'social',
-	'tooltip'		=> esc_html__( 'Font Awesome names:', 'magaziner' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'magaziner' ) . ' </strong></a>',
+	'tooltip'		=> esc_html__( 'Font Awesome names:', 'magaziner' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'magaziner' ) . ' </strong></a>',
 	'row_label'		=> array(
 		'type'	=> 'text',
 		'value'	=> esc_html__('social link', 'magaziner' ),
@@ -742,7 +742,7 @@ Kirki::add_field( 'magaziner_theme', array(
 		'social-icon'	=> array(
 			'type'			=> 'text',
 			'label'			=> esc_html__( 'Icon Name', 'magaziner' ),
-			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'magaziner' ) . ' <a href="https://fontawesome.com/icons?d=gallery&s=brands&m=free" target="_blank"><strong>' . esc_html__( 'View All', 'magaziner' ) . ' </strong></a>',
+			'description'	=> esc_html__( 'Font Awesome icons. Ex: fa-facebook ', 'magaziner' ) . ' <a href="https://fontawesome.com/v5/search?s=brands" target="_blank"><strong>' . esc_html__( 'View All', 'magaziner' ) . ' </strong></a>',
 			'default'		=> 'fa-',
 		),
 		'social-link'	=> array(
